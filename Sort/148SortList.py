484aba624ae6856d6dd2c9f0150b86dabcc2bef0
Jesse Liu <lzhliu@ucdavis.edu>
2024-04-12 23:31:29 -0400

    consistency: day 1

--- Sort/148SortList.py
+++ Sort/148SortList.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 # QUESTION: 为啥这个不行
 # class Solution:
@@ -27,37 +27,38 @@
 '''
     快慢指针来找到中点，然后分治,  assume每一个list都是sorted的, 这样可以merge
 '''    
+
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-        
-        slow, fast = head, head.next
+            if not head or not head.next:
+                return head
+            
+            slow, fast = head, head.next
 
-        while(fast and fast.next):
-            slow = slow.next
-            fast = fast.next.next
-        
-        mid = slow.next
-        slow.next = None
-        
-        left = self.sortList(head)
-        right = self.sortList(mid)
+            while(fast and fast.next):
+                slow = slow.next
+                fast = fast.next.next
+            
+            mid = slow.next
+            slow.next = None
+            
+            left = self.sortList(head)
+            right = self.sortList(mid)
 
-        dummy = ListNode(0)
-        cur = dummy
-        while(left and right):
-            if left.val < right.val:
-                cur.next = left
-                left = left.next
-            else:
-                cur.next = right
-                right = right.next
-            cur = cur.next
-        
-        cur.next = left or right
+            dummy = ListNode(0)
+            cur = dummy
+            while(left and right):
+                if left.val < right.val:
+                    cur.next = left
+                    left = left.next
+                else:
+                    cur.next = right
+                    right = right.next
+                cur = cur.next
+            
+            cur.next = left or right
 
 
-        return dummy.next
-        
+            return dummy.next
+            
         

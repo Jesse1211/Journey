c4689941b2af28a8f7bb3935193dff0a1eec00c0
Jesse Liu <zl942@cornell.edu>
2024-09-11 20:44:27 -0400

    day 55

---
+++ DataStructure/LinkedList/2807.java
@@ -0,0 +1,53 @@
+package DataStructure.LinkedList;
+
+/*
+ * highlight: findGCD比较难
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null) {
+            int gcd = findGCD(slow.val, fast.val);
+            ListNode cur = new ListNode(gcd);
+            cur.next = slow.next;
+            slow.next = cur;
+            slow = fast;
+            fast = fast.next;
+        }
+        return head;
+    }
+
+    private int findGCD(int a, int b) {
+        // Base case: if b becomes 0, the GCD is a
+        if (b == 0) {
+            return a;
+        }
+        // Recursive case: call the function again with the second number and the
+        // remainder
+        return findGCD(b, a % b);
+    }
+}
--- README.md
+++ README.md
@@ -63,3 +63,4 @@ id 刷题 === 早日上岸: why don't you start right now?
   - 今天加练 - sub 要复习完
 - [x] Day53 9/7/2024 第三 section: 跟着 zhiyuan 瞎几把刷, 搞得我头疼
 - [x] Day54 9/8/2024 第三 section: 小练
+- [x] Day55 9/9/2024 第三 section: 小练

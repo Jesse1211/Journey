24bfaa30eff5f01dcb6047b6b695d8940f6375c9
Jesse Liu <zl942@cornell.edu>
2024-09-19 23:37:40 -0400

    day 60

--- DataStructure/LinkedList/19.java
+++ DataStructure/LinkedList/19.java
@@ -17,23 +17,32 @@ class ListNode {
     }
 }
 
+/*
+ * 两个指针, two pointers
+ */
+
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode left = head;
         ListNode right = head;
+
+        // set the window
         for (int i = 0; i < n; i++) {
             right = right.next;
         }
 
+        // when remove the head -> n = length
         if (right == null) {
             return head.next;
         }
 
-        while (right.next != null) {
-            right = right.next;
+        // shift to right most
+        while (right != null && right.next != null) {
             left = left.next;
+            right = right.next;
         }
 
+        // drop the element
         left.next = left.next.next;
         return head;
 

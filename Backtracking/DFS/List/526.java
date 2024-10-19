c73ca3dc109dcecf06ab608df036ea713cf30b3c
Jesse Liu <zl942@cornell.edu>
2024-10-18 23:30:09 -0400

    add one more solution

--- Backtracking/DFS/List/526.java
+++ Backtracking/DFS/List/526.java
@@ -38,3 +38,37 @@ class Solution {
         return res;
     }
 }
+
+class BetterSolution {
+    public int countArrangement(int n) {
+        int[] nums = new int[n + 1];
+
+        for (int i = 1; i < n + 1; i++) {
+            nums[i] = i;
+        }
+
+        return dfs(nums, 1);
+    }
+
+    private int dfs(int[] nums, int index) {
+        if (nums.length == index) {
+            return 1;
+        }
+
+        int ans = 0;
+        for (int i = index; i < nums.length; i++) {
+            swap(nums, index, i);
+            if (nums[index] % index == 0 || index % nums[index] == 0) {
+                ans += dfs(nums, index + 1);
+            }
+            swap(nums, index, i);
+        }
+        return ans;
+    }
+
+    private void swap(int[] nums, int x, int y) {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
+}

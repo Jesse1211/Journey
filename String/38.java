5619e8fe2e598a19380d8706510111d46f32f921
Jesse Liu <zl942@cornell.edu>
2024-11-25 01:11:31 -0500

    day 99

---
+++ String/38.java
@@ -0,0 +1,26 @@
+package String;
+
+class Solution {
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+
+        String child = countAndSay(n - 1);
+
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        int i = 0;
+        while (i < child.length()) {
+            char cur = child.charAt(i);
+            while (i < child.length() && child.charAt(i) == cur) {
+                i++;
+                count++;
+            }
+            sb.append(count).append(cur);
+            count = 0;
+        }
+
+        return sb.toString();
+    }
+}

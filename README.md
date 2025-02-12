25f1bed3e27820921b4b7110f220c4f1e10ddb99
Jesse Liu <zl942@cornell.edu>
2025-02-12 21:14:31 +0800

    day 110

--- README.md
+++ README.md
@@ -157,3 +157,4 @@ id 刷题 === 早日上岸: why don't you start right now?
 - [x] Day107 02/07/2025 刷 Binary search
 - [x] Day108 02/09/2025 刷 Two Pointer
 - [x] Day109 02/11/2025 刷 Goldman Sachs
+- [x] Day110 02/12/2025 刷 Goldman Sachs
--- SlidingWindow/3.java
+++ SlidingWindow/3.java
@@ -5,20 +5,23 @@ import java.util.Set;
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Set<Character> visited = new HashSet<>();
-        int left = 0;
-        int right = 0;
         int res = 0;
-        while (right < s.length()) {
-            char cur = s.charAt(right++);
 
-            while (visited.contains(cur)) {
-                visited.remove(s.charAt(left++));
+        Set<Character> set = new HashSet<>();
+        int slow = 0;
+        int fast = 0;
+
+        while (fast < s.length()) {
+            while (set.contains(s.charAt(fast))) {
+                set.remove(s.charAt(slow++));
             }
-            visited.add(cur);
-            res = Math.max(right - left, res);
 
+            set.add(s.charAt(fast));
+            fast++;
+
+            res = Math.max(fast - slow, res);
         }
+
         return res;
     }
 }
